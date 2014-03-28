b7509788ec1a429543209fbc62bc33ff3c8dd6c5
Thomas Tuegel <ttuegel@gmail.com>
2014-03-28 14:22:31 +0000

    Encode matrix formats with DataKinds

--- Numeric/LinearAlgebra/Sparse.hs
+++ Numeric/LinearAlgebra/Sparse.hs
@@ -1,44 +1,46 @@
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeFamilies #-}
 
 module Numeric.LinearAlgebra.Sparse where
 
 import Control.Applicative ((<$>))
+import Control.Exception (assert)
 import Control.Monad (when)
 import Control.Monad.Primitive (PrimMonad(..))
 import Control.Monad.ST (runST)
+import Data.Maybe (fromMaybe)
+import Data.Vector.Unboxed (Unbox, Vector)
 import qualified Data.Vector.Unboxed as U
 import qualified Data.Vector.Unboxed.Mutable as MU
 import Data.Vector.Algorithms.Intro (sortBy)
 import Data.Vector.Algorithms.Search (Comparison, binarySearchP)
 
-data CS a = CS !Int !(U.Vector Int) !(U.Vector (Int, a))
-data Coord a = Coord !Int !Int !(U.Vector (Int, Int, a))
+data CS a = CS !Int !(Vector Int) !(Vector (Int, a))
+data CO a = CO !Int !Int !(Vector (Int, Int, a))
 
-data CSR = CSR
-data CSC = CSC
-data COO = COO
+data FormatK = CSR | CSC | COO
 
-type family Matrix fmt a
-type instance Matrix CSR a = CS a
-type instance Matrix CSC a = CS a
-type instance Matrix COO a = Coord a
+data family Matrix :: FormatK -> * -> *
+newtype instance Matrix CSR a = MatrixCSR (CS a)
+newtype instance Matrix CSC a = MatrixCSC (CS a)
+newtype instance Matrix COO a = MatrixCOO (CO a)
 
 class Format f where
-    coo :: Matrix f a -> Matrix COO a
-    csr :: Matrix f a -> Matrix CSR a
-    csc :: Matrix f a -> Matrix CSC a
+    coo :: Unbox a => Matrix f a -> Matrix COO a
+    csr :: Unbox a => Matrix f a -> Matrix CSR a
+    csc :: Unbox a => Matrix f a -> Matrix CSC a
 
 instance Format COO where
     coo = id
 
-    csr (Coord nRows nCols vals) =
+    csr (MatrixCOO (CO nRows nCols vals)) =
         runST $ do
           valsM <- U.thaw vals
           sortBy rowOrdering valsM
           rows <- U.generateM nRows
             $ \i -> binarySearchP (\(r, _, _) -> r == i) valsM
           vals' <- U.map (\(_, c, x) -> (c, x)) <$> U.freeze valsM
-          return $! CS nCols rows vals'
+          return $! MatrixCSR (CS nCols rows vals')
       where
         rowOrdering :: Comparison (Int, Int, a)
         rowOrdering (ra, ca, _) (rb, cb, _) =
@@ -46,14 +48,14 @@ instance Format COO where
               EQ -> compare ca cb
               x -> x
 
-    csc (Coord nRows nCols vals) =
+    csc (MatrixCOO (CO nRows nCols vals)) =
         runST $ do
           valsM <- U.thaw vals
           sortBy colOrdering valsM
           cols <- U.generateM nRows
             $ \i -> binarySearchP (\(_, c, _) -> c == i) valsM
           vals' <- U.map (\(r, _, x) -> (r, x)) <$> U.freeze valsM
-          return $! CS nRows cols vals'
+          return $! MatrixCSC (CS nRows cols vals')
       where
         colOrdering :: Comparison (Int, Int, a)
         colOrdering (ra, ca, _) (rb, cb, _) =
@@ -62,11 +64,10 @@ instance Format COO where
               x -> x
 
 instance Format CSR where
-    coo (CS nCols rows vals) =
-        Coord nRows nCols $ U.create $ do
-          let spans = U.postscanr' (\start (end, _) -> (start, end)) (len, 0) rows
+    coo (MatrixCSR (CS nCols rows vals)) =
+        MatrixCOO $ CO nRows nCols $ U.create $ do
           vals' <- U.unsafeThaw $ U.map (\(c, x) -> (-1, c, x)) vals
-          U.forM_ (U.indexed spans) $ \(r, (start, end)) ->
+          U.forM_ (U.indexed $ extents rows) $ \(r, (start, end)) ->
             let go i
                   | i < end = do
                     (_, c, x) <- MU.read vals' i
@@ -83,11 +84,10 @@ instance Format CSR where
     csc = csc . coo
 
 instance Format CSC where
-    coo (CS nRows cols vals) =
-        Coord nRows nCols $ U.create $ do
-          let spans = U.postscanr' (\start (end, _) -> (start, end)) (len, 0) cols
+    coo (MatrixCSC (CS nRows cols vals)) =
+        MatrixCOO $ CO nRows nCols $ U.create $ do
           vals' <- U.unsafeThaw $ U.map (\(r, x) -> (r, -1, x)) vals
-          U.forM_ (U.indexed spans) $ \(c, (start, end)) ->
+          U.forM_ (U.indexed $ extents cols) $ \(c, (start, end)) ->
             let go i
                   | i < end = do
                     (r, _, x) <- MU.read vals' i
@@ -103,8 +103,22 @@ instance Format CSC where
     csr = csr . coo
     csc = id
 
-mm :: (Format f, Format g) => Matrix CSR a -> Matrix CSC a -> Matrix COO a
-mv :: (Format f, U.Vector v a) => Matrix CSR a -> v a -> v a
+extents :: Vector Int -> Vector (Int, Int)
+extents ixs =
+    U.postscanr' (\start (end, _) -> (start, end)) (len, 0) ixs
+  where
+    len = U.length ixs
 
-mvM :: (Format f, PrimMonad m, MU.MVector v a)
-    => Matrix CSR a -> v (PrimState m) a -> m (v (PrimState m) a)
+mm :: Matrix CSR a -> Matrix CSC a -> Matrix COO a
+mm = undefined
+
+mv :: (Num a, Unbox a) => Matrix CSR a -> Vector a -> Vector a
+mv (MatrixCSR (CS nCols rows vals)) vec =
+    assert (nCols == U.length vec)
+    $ flip U.map (extents rows)
+    $ \(start, end) ->
+      let (cols, coeffs) = U.unzip $ U.slice start (end - start) vals
+      in U.sum $ U.zipWith (*) coeffs $ U.backpermute vec cols
+
+mvM :: Matrix CSR a -> v (PrimState m) a -> m (v (PrimState m) a)
+mvM = undefined
