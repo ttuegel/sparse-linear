f5bb3b7a67e010c51c12c5aa1c970d97f2058f15
Thomas Tuegel <ttuegel@gmail.com>
2014-04-04 12:15:23 +0000

    Move polykinded Proxy to its own module

---
+++ src/Data/Proxy/PolyKind.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE PolyKinds #-}
+
+module Data.Proxy.PolyKind where
+
+-- | PolyKind Proxy
+data Proxy p = Proxy
+
+-- | PolyKind Tagged
+newtype Tagged t a = Tagged { untag :: a }
+
+{-# INLINE tag #-}
+tag :: Proxy t -> a -> Tagged t a
+tag Proxy = Tagged
+
+{-# INLINE unproxy #-}
+unproxy :: (Proxy t -> a) -> Tagged t a
+unproxy f = let witness = Proxy in tag witness (f witness)
+
+{-# INLINE proxy #-}
+proxy :: Tagged t a -> Proxy t -> a
+proxy tagged Proxy = untag tagged
+
+{-# INLINE copyTag #-}
+copyTag :: Tagged t a -> b -> Tagged t b
+copyTag t x = untag $ unproxy $ \witness -> let _ = proxy t witness in tag witness x
+
+instance Functor (Tagged t) where
+    fmap f tagged = untag $ unproxy $ \p -> tag p $ f $ untag tagged
+
--- src/Numeric/LinearAlgebra/Sparse.hs
+++ src/Numeric/LinearAlgebra/Sparse.hs
@@ -22,30 +22,7 @@ import qualified Data.Vector.Unboxed.Mutable as MU
 import Data.Vector.Algorithms.Intro (sortBy)
 import Data.Vector.Algorithms.Search (binarySearchL)
 
--- | PolyKind Proxy
-data Proxy p = Proxy
-
--- | PolyKind Tagged
-newtype Tagged t a = Tagged { untag :: a }
-
-{-# INLINE tag #-}
-tag :: Proxy t -> a -> Tagged t a
-tag Proxy = Tagged
-
-{-# INLINE unproxy #-}
-unproxy :: (Proxy t -> a) -> Tagged t a
-unproxy f = let witness = Proxy in tag witness (f witness)
-
-{-# INLINE proxy #-}
-proxy :: Tagged t a -> Proxy t -> a
-proxy tagged Proxy = untag tagged
-
-{-# INLINE copyTag #-}
-copyTag :: Tagged t a -> b -> Tagged t b
-copyTag t x = untag $ unproxy $ \witness -> let _ = proxy t witness in tag witness x
-
-instance Functor (Tagged t) where
-    fmap f tagged = untag $ unproxy $ \p -> tag p $ f $ untag tagged
+import Data.Proxy.PolyKind
 
 -- | Matrix order
 data OrderK
