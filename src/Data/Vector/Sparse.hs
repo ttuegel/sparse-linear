f43638da230cd67846b777c6d8b1e454f7dbd146
Thomas Tuegel <ttuegel@gmail.com>
2014-12-31 15:45:54 -0600

    Data.Vector.Sparse: add MonoFoldable, cmap

--- src/Data/Vector/Sparse.hs
+++ src/Data/Vector/Sparse.hs
@@ -1,13 +1,15 @@
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE ViewPatterns #-}
 {-# OPTIONS_GHC -fsimpl-tick-factor=200 #-}
 
 module Data.Vector.Sparse
-       ( Vector(..)
+       ( Vector(..), cmap
        , fromPairs, (|>)
        ) where
 
 import Control.Monad.ST (runST)
+import Data.MonoTraversable
 import Data.Ord (comparing)
 import qualified Data.Vector.Algorithms.Intro as Intro
 import Data.Vector.Storable (Storable)
@@ -35,3 +37,25 @@ fromPairs = \dim _pairs -> runST $ do
 (|>) :: (Storable a, Unbox a) => Int -> [(Int, a)] -> Vector a
 {-# INLINE (|>) #-}
 (|>) = \dim pairs -> fromPairs dim $ U.fromList pairs
+
+type instance Element (Vector a) = a
+
+instance Storable a => MonoFunctor (Vector a) where
+  omap = \f v -> v { values = omap f $ values v }
+  {-# INLINE omap #-}
+
+instance Storable a => MonoFoldable (Vector a) where
+  ofoldMap = \f Vector{..} -> ofoldMap f values
+  {-# INLINE ofoldMap #-}
+  ofoldr = \f r Vector{..} -> ofoldr f r values
+  {-# INLINE ofoldr #-}
+  ofoldl' = \f r Vector{..} -> ofoldl' f r values
+  {-# INLINE ofoldl' #-}
+  ofoldr1Ex = \f Vector{..} -> ofoldr1Ex f values
+  {-# INLINE ofoldr1Ex #-}
+  ofoldl1Ex' = \f Vector{..} -> ofoldl1Ex' f values
+  {-# INLINE ofoldl1Ex' #-}
+
+cmap :: (Storable a, Storable b) => (a -> b) -> Vector a -> Vector b
+{-# INLINE cmap #-}
+cmap = \f v -> v { values = V.map f $ values v }
