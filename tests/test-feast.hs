5bb6bce66c9c617173efc03309fabf3fa946b4e4
Thomas Tuegel <ttuegel@gmail.com>
2015-01-06 13:02:55 -0600

    test-feast: actually catch errors in test suite

--- tests/test-feast.hs
+++ tests/test-feast.hs
@@ -14,6 +14,7 @@ main = hspec $ do
     it "ident" $ do
       let m :: Matrix Col (Complex Double)
           m = fromTriples 2 2 [(0, 0, 2), (0, 1, -1), (1, 0, -1), (1, 1, 2)]
-          eigenvalues = fst $ eigSH 2 (-5, 5) m
-          res = V.map abs $ V.zipWith (-) eigenvalues (V.fromList [1, 3])
-      V.all (< 1E-12) res `shouldBe` True
+          eigenvalues = fst $ eigSH 2 (0, 4) m
+          correct = V.fromList [1, 3]
+      V.length eigenvalues `shouldBe` V.length correct
+      V.all (< 1E-12) (V.map abs $ V.zipWith (-) eigenvalues correct) `shouldBe` True
