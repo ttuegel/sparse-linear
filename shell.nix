af828860d0f7604778544a21d9e97923a04cc4a0
Thomas Tuegel <ttuegel@gmail.com>
2015-01-06 12:40:38 -0600

    default.nix: remove global-lock dependency

--- shell.nix
+++ shell.nix
@@ -4,6 +4,5 @@ with pkgs;
 
 haskellPackages.callPackage ./. {
   suitesparse = suitesparse_4_4_1;
-  globalLock = haskellPackages.callPackage ./global-lock.nix {};
   sparseLinear = haskellPackages.callPackage ../sparse-linear {};
 }
