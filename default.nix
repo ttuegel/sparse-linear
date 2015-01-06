502c8bbb5e3d710409d796c7d032af26771e0607
Thomas Tuegel <ttuegel@gmail.com>
2015-01-06 10:22:48 -0600

    default.nix: fix package name, description

--- default.nix
+++ default.nix
@@ -2,14 +2,14 @@
 , tagged, vector }:
 
 cabal.mkDerivation (self: {
-  pname = "suitesparse";
+  pname = "sparse-linear";
   version = "0.1.0.0";
   src = ./.;
   buildDepends = [ monoTraversable storableComplex tagged vector ];
   testDepends = [ hspec QuickCheck vector ];
   extraLibraries = [ ];
   meta = {
-    description = "Haskell bindings to the SuiteSparse library of sparse linear algebra routines";
+    description = "Sparse linear algebra primitives in Haskell";
     license = self.stdenv.lib.licenses.gpl2;
     platforms = self.ghc.meta.platforms;
   };
