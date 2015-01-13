6e03c7f10f486375e1a3c51b117c4b4b86fbee8f
Thomas Tuegel <ttuegel@gmail.com>
2015-01-13 15:10:29 -0600

    Feast: separate foreign bits

--- src/Numeric/LinearAlgebra/Feast.hs
+++ src/Numeric/LinearAlgebra/Feast.hs
@@ -3,249 +3,169 @@
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE ForeignFunctionInterface #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE UnboxedTuples #-}
 
 module Numeric.LinearAlgebra.Feast
-    ( EigSH, geigSH, eigSH
+    ( Feast, geigSH, eigSH
     ) where
 
 import Control.Applicative
 import Control.Concurrent.ParallelIO.Local (withPool, parallel_)
 import Control.Monad (when)
-import Data.Foldable
 import Data.IORef
 import Data.Traversable
 import Data.Vector.Storable (Vector)
 import qualified Data.Vector.Storable as V
 import qualified Data.Vector.Storable.Mutable as MV
-import Foreign.C.Types
-import Foreign.Marshal.Array (withArray)
 import Foreign.Marshal.Utils (with)
-import Foreign.Ptr
 import Foreign.Storable
 import GHC.Conc.Sync (getNumCapabilities)
 import GHC.Stack (errorWithStackTrace)
-import Prelude hiding (concat, mapM)
+import Prelude hiding (concat, error, mapM)
 import System.GlobalLock (lock)
 import System.IO.Unsafe
 
 import Data.Complex.Enhanced
 import qualified Data.Packed.Matrix as Dense
-import qualified Data.Matrix.Sparse as Sparse
+import Data.Matrix.Sparse
+import Numeric.LinearAlgebra.Feast.Internal
 import Numeric.LinearAlgebra.Umfpack
 
-type FeastRci a
-    =  Ptr CInt  -- ^ ijob
-    -> Ptr CInt  -- ^ N
-    -> Ptr a  -- ^ Ze
-    -> Ptr a  -- ^ work
-    -> Ptr (ComplexOf a)  -- ^ workc
-    -> Ptr a  -- ^ Aq
-    -> Ptr a  -- ^ Sq
-    -> Ptr CInt  -- ^ feastparam
-    -> Ptr (RealOf a)  -- ^ epsout
-    -> Ptr CInt  -- ^ loop
-    -> Ptr (RealOf a)  -- ^ Emin
-    -> Ptr (RealOf a)  -- ^ Emax
-    -> Ptr CInt  -- ^ M0
-    -> Ptr (RealOf a)  -- ^ lambda
-    -> Ptr a  -- ^ q
-    -> Ptr CInt  -- ^ mode
-    -> Ptr (RealOf a)  -- ^ res
-    -> Ptr CInt  -- ^ info
-    -> IO ()
+error :: String -> a
+error = errorWithStackTrace
 
-class Feast a where
-    feast_rci :: FeastRci a
-
-foreign import ccall "feastinit_" feastinit :: Ptr CInt -> IO ()
-
-foreign import ccall "zfeast_hrci_" zfeast_hrci :: FeastRci (Complex Double)
-
-foreign import ccall "dfeast_srci_" dfeast_srci :: FeastRci Double
-
-instance Feast (Complex Double) where
-    feast_rci = zfeast_hrci
-    {-# INLINE feast_rci #-}
-
-instance Feast Double where
-    feast_rci = dfeast_srci
-    {-# INLINE feast_rci #-}
-
--- | Split a list into chunks with the given length.
-chunks :: Int -> [a] -> [[a]]
-{-# INLINE chunks #-}
-chunks n xs = chunks_go 0 xs where
-  (q, r) = length xs `divMod` n
-  chunks_go i ys =
-    case ys of
-      [] -> []
-      _ ->
-        let (this, rest) = splitAt (if i < r then q + 1 else q) ys
-        in this : chunks_go (i + 1) rest
-
-type EigSH a =
-    ( Eq a
-    , Feast a
-    , IsReal a
-    , Num (RealOf a)
-    , Show a
-    , Storable (RealOf a)
-    , Umfpack a
-    , Dense.Element a
-    )
 geigSH
-  :: (EigSH a)
+  :: (Feast a)
   => Int -> (RealOf a, RealOf a)
-  -> Sparse.Matrix Sparse.Col a -> Sparse.Matrix Sparse.Col a
+  -> Matrix Col a -> Matrix Col a
   -> (Vector (RealOf a), Dense.Matrix a)
-geigSH !m0 (!_emin, !_emax) !matA !matB
-  | matA /= matA' = errorWithStackTrace "matrix A must be hermitian"
-  | matB /= matB' = errorWithStackTrace "matrix B must be hermitian"
-  | Sparse.odim matA /= Sparse.odim matB =
-      errorWithStackTrace "matrices A and B must be the same size"
-  | otherwise = unsafePerformIO $ lock $ do
-      nCap <- getNumCapabilities
-      withPool nCap (geigSH_go nCap)
-  where
-    n = Sparse.odim matA
-    matA' = Sparse.reorient $ Sparse.ctrans matA
-    matB' = Sparse.reorient $ Sparse.ctrans matB
-
-    {-# NOINLINE geigSH_go #-}
-    geigSH_go nCap pool =
-      -- initialize scalars
-      with (-1) $ \_ijob ->
-      withArray (replicate 64 0) $ \fpm ->
-      with (fromIntegral n) $ \n_ ->
-      with 0 $ \_ze ->
-      with 0 $ \epsout ->
-      with 0 $ \loop ->
-      with _emin $ \_emin -> with _emax $ \_emax ->
-      with (fromIntegral m0) $ \m0_ ->
-      with 0 $ \mode ->
-      with 0 $ \info -> do
-
-        -- initialize vectors
-        _eigenvalues <- MV.replicate m0 0
-        _eigenvectors <- MV.replicate (m0 * n) 0
-        _work1 <- MV.replicate (m0 * n) 0
-        _work2 <- MV.replicate (m0 * n) 0
-        _aq <- MV.replicate (m0 * m0) 0
-        _bq <- MV.replicate (m0 * m0) 0
-        _res <- MV.replicate m0 0
-
-        -- initialize
-        feastinit fpm
-
-        let feast_go =
-              MV.unsafeWith _work1 $ \_work1 ->
-              MV.unsafeWith _work2 $ \_work2 ->
-              MV.unsafeWith _aq $ \_aq ->
-              MV.unsafeWith _bq $ \_bq ->
-              MV.unsafeWith _eigenvalues $ \_eigenvalues ->
-              MV.unsafeWith _eigenvectors $ \_eigenvectors ->
-              MV.unsafeWith _res $ \_res ->
-                feast_rci
-                  _ijob
-                  n_
-                  _ze
-                  _work1
-                  _work2
-                  _aq
-                  _bq
-                  fpm
-                  epsout
-                  loop
-                  _emin
-                  _emax
-                  m0_
-                  _eigenvalues
-                  _eigenvectors
-                  mode
-                  _res
-                  info
-
-        _work1 <- forM [0..(m0 - 1)] $ \c ->
-          return $ MV.slice (c * n) n _work1
-        _work2 <- forM [0..(m0 - 1)] $ \c ->
-          return $ MV.slice (c * n) n _work2
-        _eigenvectors <- forM [0..(m0 - 1)] $ \c ->
-          return $ MV.slice (c * n) n _eigenvectors
-
-        _fact <- newIORef undefined
-        _mat <- newIORef undefined
-
-        let go = do
-              feast_go
-              _ijob <- peek _ijob
-              when (_ijob /= 0) $ do
-                case _ijob of
-                 10 -> do
-                   _ze <- peek _ze
-                   let mat = Sparse.lin (-1) matA _ze matB
-                   writeIORef _mat $! mat
-                   writeIORef _fact $! factor mat
-                 11 -> solveLinear UmfpackNormal
-                 20 -> return ()
-                 21 -> solveLinear UmfpackTrans
-                 30 -> multiplyWork matA
-                 40 -> multiplyWork matB
-                 _ -> return ()
-                go
-
-            parMapM_ :: (a -> IO b) -> [a] -> IO ()
-            parMapM_ f xs = parallel_ pool $ map f xs
-
-            solveLinear m = do
-              !fact <- readIORef _fact
-              !mat <- readIORef _mat
-              let solveLinear_go chunk = do
-                    solns <- linearSolve_ fact m mat chunk
-                    forM_ (zip chunk solns) $ uncurry MV.copy
-              parMapM_ solveLinear_go (chunks nCap _work2)
-
-            multiplyWork mat = do
-              i <- (+ (-1)) . fromIntegral <$> peekElemOff fpm 23
-              j <- fromIntegral <$> peekElemOff fpm 24
-              let range = take j $ drop i $ zip _work1 _eigenvectors
-                  multiplyWork_go chunk = forM_ chunk $ \(!dst, !x) -> do
-                    MV.set dst 0
-                    Sparse.gaxpy_ mat x dst
-              parMapM_ multiplyWork_go (chunks nCap range)
-        go
-
-        i <- peek info
-        case i of
-          (-3) -> errorWithStackTrace
-            "geigSH: internal error in reduced eigenvalue solver"
-          (-2) -> errorWithStackTrace
-            "geigSH: internal error in inner system solver"
-          (-1) -> errorWithStackTrace
-            "geigSH: internal error in memory allocation"
-          0 -> return ()
-          1 -> putStrLn "geigSH: no eigenvalues in search interval"
-          2 -> putStrLn "geigSH: no convergence"
-          3 -> putStrLn "geigSH: subspace too small"
-          4 -> putStrLn "geigSH: only subspace returned"
-          _ -> errorWithStackTrace
-               $ "geigSH: error info = " ++ show i
-
-        m <- fromIntegral <$> peek mode
-        _eigenvalues <- V.unsafeFreeze $ MV.slice 0 m _eigenvalues
-        _eigenvectors <- mapM V.unsafeFreeze $ take m _eigenvectors
-
-        return (_eigenvalues, Dense.fromColumns _eigenvectors)
 {-# INLINE geigSH #-}
+geigSH !m0 (!_emin, !_emax) !matA !matB = geigSH_go where
+  n = odim matA
+
+  {-# NOINLINE geigSH_go #-}
+  geigSH_go
+    | not (hermitian matA) = error "geigSH: matrix A not hermitian"
+    | not (hermitian matB) = error "geigSH: matrix B not hermitian"
+    | odim matA /= odim matB = error "geigSH: matrix sizes do not match"
+    | otherwise = unsafePerformIO $ lock $ do
+        ncap <- getNumCapabilities
+        withPool ncap $ \pool ->
+
+          -- initialize scalars
+          with (-1) $ \_ijob ->
+          with (fromIntegral n) $ \n_ ->
+          with 0 $ \_ze ->
+          with 0 $ \epsout ->
+          with 0 $ \loop ->
+          with _emin $ \_emin ->
+          with _emax $ \_emax ->
+          with (fromIntegral m0) $ \m0_ ->
+          with 0 $ \mode ->
+          with 0 $ \info -> do
+
+            -- initialize vectors
+            _eigenvalues <- MV.replicate m0 0
+            _eigenvectors <- MV.replicate (m0 * n) 0
+            _work1 <- MV.replicate (m0 * n) 0
+            _work2 <- MV.replicate (m0 * n) 0
+            _aq <- MV.replicate (m0 * m0) 0
+            _bq <- MV.replicate (m0 * m0) 0
+            _res <- MV.replicate m0 0
+
+            -- initialize
+            fpm <- feastinit
+
+            let feast_go =
+                  MV.unsafeWith fpm $ \_fpm ->
+                  MV.unsafeWith _work1 $ \_work1 ->
+                  MV.unsafeWith _work2 $ \_work2 ->
+                  MV.unsafeWith _aq $ \_aq ->
+                  MV.unsafeWith _bq $ \_bq ->
+                  MV.unsafeWith _eigenvalues $ \_eigenvalues ->
+                  MV.unsafeWith _eigenvectors $ \_eigenvectors ->
+                  MV.unsafeWith _res $ \_res ->
+                    feast_rci
+                      _ijob n_ _ze _work1 _work2 _aq _bq
+                      _fpm epsout loop _emin _emax
+                      m0_ _eigenvalues _eigenvectors
+                      mode _res info
+
+            let sliceMat mat =
+                  map
+                    (\c -> MV.unsafeSlice (c * n) n mat)
+                    [0..(m0 - 1)]
+            _work1 <- return (sliceMat _work1)
+            _work2 <- return (sliceMat _work2)
+            _eigenvectors <- return (sliceMat _eigenvectors)
+
+            _fact <- newIORef undefined
+            _mat <- newIORef undefined
+
+            let go = do
+                  feast_go
+                  _ijob <- peek _ijob
+                  when (_ijob /= 0) $ do
+                    case _ijob of
+                     10 -> do
+                       _ze <- peek _ze
+                       let mat = lin (-1) matA _ze matB
+                       writeIORef _mat $! mat
+                       writeIORef _fact $! factor mat
+                     11 -> solveLinear UmfpackNormal
+                     20 -> return ()
+                     21 -> solveLinear UmfpackTrans
+                     30 -> multiplyWork matA
+                     40 -> multiplyWork matB
+                     _ -> return ()
+                    go
+
+                parMapM_ :: (a -> IO b) -> [a] -> IO ()
+                parMapM_ f xs = parallel_ pool $ map f xs
+
+                solveLinear m = do
+                  !fact <- readIORef _fact
+                  !mat <- readIORef _mat
+                  parMapM_
+                    (\work -> do
+                         [soln] <- linearSolve_ fact m mat [work]
+                         MV.copy work soln)
+                    _work2
+
+                multiplyWork mat = do
+                  ndrop <- (+ (-1)) . fromIntegral <$> MV.unsafeRead fpm 23
+                  ntake <- fromIntegral <$> MV.unsafeRead fpm 24
+                  parMapM_
+                    (\(!dst, !x) -> MV.set dst 0 >> gaxpy_ mat x dst)
+                    (take ntake (drop ndrop (zip _work1 _eigenvectors)))
+            go
+
+            i <- peek info
+            case i of
+              (-3) ->
+                error "geigSH: internal error in reduced eigenvalue solver"
+              (-2) -> error "geigSH: internal error in inner system solver"
+              (-1) -> error "geigSH: internal error in memory allocation"
+              0 -> return ()
+              1 -> putStrLn "geigSH: no eigenvalues in search interval"
+              2 -> putStrLn "geigSH: no convergence"
+              3 -> putStrLn "geigSH: subspace too small"
+              4 -> putStrLn "geigSH: only subspace returned"
+              _ -> error ("geigSH: unknown error, info = " ++ show i)
+
+            m <- fromIntegral <$> peek mode
+            _eigenvalues <- V.unsafeFreeze $ MV.slice 0 m _eigenvalues
+            _eigenvectors <- mapM V.unsafeFreeze $ take m _eigenvectors
+
+            return (_eigenvalues, Dense.fromColumns _eigenvectors)
+
 
 eigSH
-  :: (EigSH a)
+  :: (Feast a)
   => Int
   -> (RealOf a, RealOf a)
-  -> Sparse.Matrix Sparse.Col a
+  -> Matrix Col a
   -> (Vector (RealOf a), Dense.Matrix a)
-eigSH = \m0 bounds matA ->
-  geigSH m0 bounds matA $ Sparse.ident $ Sparse.odim matA
 {-# INLINE eigSH #-}
+eigSH = \m0 bounds matA -> geigSH m0 bounds matA (ident (odim matA))
