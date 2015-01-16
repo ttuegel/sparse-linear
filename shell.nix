ad2154335cfee3b78b2ddf8af1b84785d686df93
Thomas Tuegel <ttuegel@gmail.com>
2015-01-15 18:59:47 -0600

    use haskell-ng

--- shell.nix
+++ shell.nix
@@ -1,19 +1,22 @@
-{ pkgs ? (import <nixpkgs> {}) }:
-
-with pkgs;
+with (import <nixpkgs> {});
 let
   atlas = atlasWithLapack;
   gfortran = pkgs.gfortran.gcc;
-  lapack = atlasWithLapack;
-  ptcblas = atlasWithLapack;
-  ptf77blas = atlasWithLapack;
-  sparseLinear = haskellPackages.callPackage ./sparse-linear {};
-in
-haskellPackages.callPackage ./. {
-  inherit atlas lapack gfortran ptcblas ptf77blas sparseLinear;
-  suitesparse = haskellPackages.callPackage ./suitesparse {
-    inherit sparseLinear;
-    suitesparse = suitesparse_4_4_1;
+  lapack = null;
+  ptcblas = null;
+  ptf77blas = null;
+  sparse-linear = haskellngPackages.callPackage ./sparse-linear {};
+  suitesparse = haskellngPackages.callPackage ./suitesparse {
+    inherit sparse-linear;
+    amd = null;
+    cholmod = null;
+    colamd = null;
+    suitesparseconfig = suitesparse_4_4_1;
+    umfpack = null;
   };
-  globalLock = haskellPackages.callPackage ./global-lock.nix {};
-}
+  global-lock = haskellngPackages.callPackage ./global-lock.nix {};
+in
+(haskellngPackages.callPackage ./. {
+  inherit atlas lapack gfortran global-lock ptcblas ptf77blas sparse-linear
+    suitesparse;
+}).env
