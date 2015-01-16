d195c9055404e348b97bf6b332d31b645af204a9
Thomas Tuegel <ttuegel@gmail.com>
2015-01-16 10:27:08 -0600

    separate symbolic and numeric phases

--- src/Numeric/LinearAlgebra/Umfpack.hs
+++ src/Numeric/LinearAlgebra/Umfpack.hs
@@ -3,10 +3,13 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Numeric.LinearAlgebra.Umfpack
-    ( Umfpack()
-    , Factors(..), factor
-    , UmfpackMode(..)
-    , linearSolve, linearSolve_, (<\>)
+    ( -- * Simple interface
+      Umfpack()
+    , linearSolve, (<\>)
+      -- * Advanced interface
+    , Analysis, analyze
+    , Factors, factor
+    , UmfpackMode(..), linearSolve_
     ) where
 
 import Control.Applicative
@@ -28,33 +31,56 @@ import Data.Matrix.Sparse.Foreign
 import Foreign.ForeignPtr (ForeignPtr, newForeignPtr, withForeignPtr)
 import Numeric.LinearAlgebra.Umfpack.Internal
 
-data Factors a =
-  Factors
-  { fsym :: !(ForeignPtr (Symbolic a))
-  , fnum :: !(ForeignPtr (Numeric a))
-  }
+-- ------------------------------------------------------------------------
+-- Simple interface
+-- ------------------------------------------------------------------------
 
-withNum :: Factors a -> (Numeric a -> IO b) -> IO b
-withNum Factors{..} f = withForeignPtr fnum $ \p -> peek p >>= f
+linearSolve :: (Vector v a, Umfpack a) => Matrix Col a -> [v a] -> [v a]
+{-# INLINE linearSolve #-}
+linearSolve mat@Matrix{..} bs =
+  unsafePerformIO $ do
+    let fact = factor mat (analyze mat)
+    xs <- forM bs $ \_b -> do
+      _b <- V.unsafeThaw (V.convert _b)
+      linearSolve_ fact UmfpackNormal mat _b
+    map V.convert <$> mapM V.unsafeFreeze xs
 
-factor :: Umfpack a => Matrix Col a -> Factors a
-{-# NOINLINE factor #-}
-factor mat = unsafePerformIO $ withConstMatrix mat $ \m n p i x -> do
+(<\>) :: (Vector v a, Umfpack a) => Matrix Col a -> v a -> v a
+{-# INLINE (<\>) #-}
+(<\>) mat b = head $ linearSolve mat [b]
+
+-- ------------------------------------------------------------------------
+-- Advanced interface
+-- ------------------------------------------------------------------------
+
+newtype Analysis a = Analysis { fsym :: ForeignPtr (Symbolic a) }
+
+newtype Factors a = Factors { fnum :: ForeignPtr (Numeric a) }
+
+analyze :: Umfpack a => Matrix Col a -> Analysis a
+{-# INLINE analyze #-}
+analyze mat = unsafePerformIO $ withConstMatrix mat $ \m n p i x -> do
   sym <- malloc
   _stat <- umfpack_symbolic m n p i x sym nullPtr nullPtr
   fsym <- newForeignPtr umfpack_free_symbolic sym
   umfpack_report_status mat nullPtr _stat
-  when (_stat < 0) $ errorWithStackTrace "linearSolve_: umfpack_symbolic failed"
+  when (_stat < 0) $ errorWithStackTrace "analyze: umfpack_symbolic failed"
 
-  num <- malloc
-  _stat <- withForeignPtr fsym $ \_sym -> do
-    _sym <- peek _sym
-    umfpack_numeric p i x _sym num nullPtr nullPtr
-  fnum <- newForeignPtr umfpack_free_numeric num
-  umfpack_report_status mat nullPtr _stat
-  when (_stat < 0) $ errorWithStackTrace "linearSolve_: umfpack_numeric failed"
+  return Analysis{..}
 
-  return Factors{..}
+factor :: Umfpack a => Matrix Col a -> Analysis a -> Factors a
+{-# INLINE factor #-}
+factor mat Analysis{..} =
+  unsafePerformIO $ withConstMatrix mat $ \_ _ p i x -> do
+    num <- malloc
+    _stat <- withForeignPtr fsym $ \_sym -> do
+      _sym <- peek _sym
+      umfpack_numeric p i x _sym num nullPtr nullPtr
+    fnum <- newForeignPtr umfpack_free_numeric num
+    umfpack_report_status mat nullPtr _stat
+    when (_stat < 0) $ errorWithStackTrace "factor: umfpack_numeric failed"
+
+    return Factors{..}
 
 data UmfpackMode = UmfpackNormal | UmfpackTrans
 
@@ -75,16 +101,10 @@ linearSolve_ fact mode mat@Matrix{..} _b =
       when (_stat < 0) $ errorWithStackTrace "linearSolve_: umfpack_solve failed"
     return _soln
 
-linearSolve :: (Vector v a, Umfpack a) => Matrix Col a -> [v a] -> [v a]
-{-# NOINLINE linearSolve #-}
-linearSolve mat@Matrix{..} bs =
-  unsafePerformIO $ do
-    let fact = factor mat
-    xs <- forM bs $ \_b -> do
-      _b <- V.unsafeThaw (V.convert _b)
-      linearSolve_ fact UmfpackNormal mat _b
-    map V.convert <$> mapM V.unsafeFreeze xs
+-- ------------------------------------------------------------------------
+-- Utilities
+-- ------------------------------------------------------------------------
 
-(<\>) :: (Vector v a, Umfpack a) => Matrix Col a -> v a -> v a
-{-# INLINE (<\>) #-}
-(<\>) mat b = head $ linearSolve mat [b]
+withNum :: Factors a -> (Numeric a -> IO b) -> IO b
+{-# INLINE withNum #-}
+withNum Factors{..} f = withForeignPtr fnum $ \p -> peek p >>= f
