fb05d52f9bb9f5b3293be9e1f69f7dee48a44c23
Thomas Tuegel <ttuegel@gmail.com>
2015-01-20 13:04:43 -0600

    nix: enable optimization and profiling

--- shell.nix
+++ shell.nix
@@ -1,13 +1,20 @@
 with (import <nixpkgs> {});
+with stdenv.lib;
 let
+  enableOptimization = ttuegel.enableOptimization or id;
+  enableProfiling = ttuegel.enableProfiling or id;
   amd = null;
   cholmod = null;
   colamd = null;
   sparse-linear = haskellngPackages.callPackage ./sparse-linear {};
   suitesparseconfig = suitesparse_4_4_1;
   umfpack = null;
-  suitesparse = haskellngPackages.callPackage ./. {
+  pkg = haskellngPackages.callPackage ./. {
     inherit amd cholmod colamd sparse-linear suitesparseconfig umfpack;
   };
 in
-suitesparse.env
+  (fold (f: x: f x) pkg
+    [
+      enableOptimization
+      enableProfiling
+    ]).env
