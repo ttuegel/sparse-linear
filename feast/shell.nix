d32ce77004717d1b12b72bc45bbc26b90c1c1345
Thomas Tuegel <ttuegel@gmail.com>
2015-01-21 14:45:41 -0600

    nix: remove profiling and optimization from shell expressions

--- feast/shell.nix
+++ feast/shell.nix
@@ -1,11 +1,7 @@
 with (import <nixpkgs> {});
-with haskell-ng.lib;
-with stdenv.lib;
 let
-  enableOptimization = ttuegel.haskell.enableOptimization or id;
-  enableProfiling = ttuegel.haskell.enableProfiling or id;
-  sparse-linear = haskellngPackages.callPackage ./sparse-linear {};
-  suitesparse = haskellngPackages.callPackage ./suitesparse {
+  sparse-linear = haskellngPackages.callPackage ../sparse-linear {};
+  suitesparse = haskellngPackages.callPackage ../suitesparse {
     inherit sparse-linear;
     amd = null;
     cholmod = null;
@@ -13,7 +9,8 @@ let
     suitesparseconfig = suitesparse_4_4_1;
     umfpack = null;
   };
-  pkg = haskellngPackages.callPackage
+in
+  (haskellngPackages.callPackage
     ./.
     {
       inherit sparse-linear suitesparse;
@@ -22,11 +19,4 @@ let
       ptcblas = null;
       lapack = null;
       gfortran = gfortran.gcc;
-      global-lock = haskellngPackages.callPackage ./global-lock.nix {};
-    };
-in
-  (fold (f: x: f x) pkg
-    [
-      enableOptimization
-      enableProfiling
-    ]).env
+    }).env
--- sparse-linear/shell.nix
+++ sparse-linear/shell.nix
@@ -1,14 +1,2 @@
 with (import <nixpkgs> {});
-with stdenv.lib;
-with haskell-ng.lib;
-let
-  enableOptimization = ttuegel.haskell.enableOptimization or id;
-  enableProfiling = ttuegel.haskell.enableProfiling or id;
-  pkg =
-    fold (f: x: f x) (haskellngPackages.callPackage ./. {})
-      [
-        enableOptimization
-        enableProfiling
-      ];
-in
-  pkg.env
+(haskellngPackages.callPackage ./. {}).env
