dc5a77655f882e60de88f8a86f879fc42c0e55c4
Thomas Tuegel <ttuegel@gmail.com>
2015-02-21 14:39:55 -0600

    feast: remove global-lock.nix

--- feast/global-lock.nix
+++
@@ -1,9 +0,0 @@
-{ mkDerivation, base, stdenv }:
-mkDerivation {
-  pname = "global-lock";
-  version = "0.1";
-  sha256 = "0b2sz9ag6wcr0amgrx08l7924brfansnh9rv64wg9s3nk4ni2sxp";
-  buildDepends = [ base ];
-  description = "A global lock implemented without unsafePerformIO";
-  license = stdenv.lib.licenses.bsd3;
-}
