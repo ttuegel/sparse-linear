4f3ade3ffccf36f34fbd38b917b9b05139d829bf
Thomas Tuegel <ttuegel@gmail.com>
2015-08-11 17:58:16 -0500

    suitesparse: use openblasCompat

--- feast/shell.nix
+++ feast/shell.nix
@@ -2,7 +2,10 @@ with (import <nixpkgs> {});
 let
   parallel-io-simple = haskellngPackages.callPackage ./parallel-io-simple {};
   sparse-linear = haskellngPackages.callPackage ../sparse-linear {};
-  suitesparse = haskellngPackages.callPackage ../suitesparse { inherit sparse-linear; };
+  suitesparse = haskellngPackages.callPackage ../suitesparse {
+    inherit sparse-linear;
+    openblas = openblasCompat;
+  };
 in
 (haskellngPackages.callPackage ./. {
   inherit parallel-io-simple sparse-linear suitesparse;
--- suitesparse/shell.nix
+++ suitesparse/shell.nix
@@ -2,4 +2,5 @@ with (import <nixpkgs> {});
 (haskellngPackages.callPackage ./. {
   sparse-linear = haskellngPackages.callPackage ../sparse-linear {};
   suitesparse = suitesparse_4_4;
+  openblas = openblasCompat;
 }).env
