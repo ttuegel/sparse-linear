61dbf978e552bff608a71b7fee9f9ca35f9e09e8
Thomas Tuegel <ttuegel@gmail.com>
2015-09-03 07:43:03 -0500

    ScatterGather: conditionally import Control.Applicative

--- sparse-linear/src/Data/Vector/Sparse/ScatterGather.hs
+++ sparse-linear/src/Data/Vector/Sparse/ScatterGather.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE RankNTypes #-}
@@ -9,6 +10,9 @@ module Data.Vector.Sparse.ScatterGather
        , gather
        ) where
 
+#if __GLASGOW_HASKELL__ < 710
+import Control.Applicative
+#endif
 import Control.Monad.ST ( ST, runST )
 import Control.Monad.Trans.Class ( lift )
 import Control.Monad.Trans.Reader ( ReaderT(..), ask )
